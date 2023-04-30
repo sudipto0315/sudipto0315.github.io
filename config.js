0c2a491141fdf714b0d4d4d8b94b2034f1e3e025
sudipto0315 <sudiptoray2003@gmail.com>
2023-04-30 11:58:30 +0530

    Updated some URLs

--- config.js
+++ config.js
@@ -458,8 +458,8 @@ window.CONFIG = {
       ],
       icon: 'amazon',
       key: 'KeyA',
-      url: 'https://www.amazon.com/',
-      normalisedURL: 'amazon.com',
+      url: 'https://www.amazon.in/',
+      normalisedURL: 'amazon.in',
       bgColor: {
         type: 'gradient',
         gradientType: 'linear',
